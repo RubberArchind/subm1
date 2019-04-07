baa826a1565c2204e53ccab2c2098b40185d2366
GIRINGULAN <35050952+RubberArchind@users.noreply.github.com>
2019-04-06 19:25:29 -0700

    Update index.php
    xm4

--- index.php
+++ index.php
@@ -5,7 +5,34 @@
 </head>
 <body>
 <div class="main">
-	<h1>ssadsada</h1>
+	<a href="proses.php?hl=add-inven"><button>Tambah+</button></a>
+	<table border="1">
+		<tr>
+			<th>No.</th><th>Username</th><th>Password</th>
+		</tr>
+			<?php
+		include 'server.php';
+			$no=1;
+      $sql = "SELECT * FROM [dbo].[user] ORDER BY username";
+      $stmt = sqlsrv_query($conn, $sql);
+      if($stmt === false)
+      {
+      die(print_r(sqlsrv_errors(), true));
+		}else{
+			while ($data=sqlsrv_fetch_array($stmt) {
+				echo "
+				<tr>
+				<td>$no</td>
+				<td>".$data['username']."</td>
+				<td>".$data['password']."</td>
+				";
+
+				$no++;
+			}
+		}
+			 ?>
+
+	</table>
 </div>
 </body>
 </html>
