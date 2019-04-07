adce1e20873848c2e4d5fb1a4ac2bbd37f0d691a
RubberArchind <sean.makuto@gmail.com>
2019-04-06 18:23:06 -0700

    cm3

--- proses.php
+++ proses.php
@@ -4,8 +4,10 @@ include 'server.php';
 switch ($_REQUEST['a']) {
   case 'add-data':
     // code...
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
     $query = $hook->prepare("INSERT INTO [dbo].[user] (username,password) VALUES username=?,  password=?");
-    $run = $query->execute([$_REQUEST['username'],$_REQUEST['password']]);
+    $run = $query->execute($user,$pass);
     if($run){
       echo "success";
     }else {
